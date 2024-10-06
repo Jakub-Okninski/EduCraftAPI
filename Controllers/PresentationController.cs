a2c786fe4195b45e8459c4d3c9ac6c90c91db77b
Jakub-Okninski <jakub.okninski@onet.pl>
2024-10-06 15:57:47 +0200

    zapias odczyt prezentacji

---
+++ Controllers/PresentationController.cs
@@ -0,0 +1,153 @@
+﻿using EduCraftAPI.Entities.Presentation;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Xml.Serialization;
+
+namespace EduCraftAPI.Controllers
+{
+    public class PresentationController : Controller
+    {
+        public void SavePresentationToXml(Presentation presentation, string filePath)
+        {
+            var xmlSerializer = new XmlSerializer(typeof(Presentation));
+
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            using (var writer = new StreamWriter(stream))
+            {
+                xmlSerializer.Serialize(writer, presentation);
+            }
+        }
+        private void ReplaceNewLineCharacters(Presentation presentation)
+        {
+            if (presentation.Slides != null)
+            {
+                foreach (var slide in presentation.Slides)
+                {
+                    if (slide.Title != null)
+                    {
+                        slide.Title = slide.Title.Replace("&#10;", "\n");
+                    }
+
+                    if (slide.Elements != null)
+                    {
+                        foreach (var element in slide.Elements)
+                        {
+                            if (element.Ops != null)
+                            {
+                                foreach (var op in element.Ops)
+                                {
+                                    if (op.Insert != null)
+                                    {
+                                       // op.InsertDecode = op.Insert.Replace("&#10;", "\n");
+                                    }
+                                }
+                            }
+
+                            if (element.Url != null)
+                            {
+                                element.Url = element.Url.Replace("&#10;", "\n");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+            public Presentation LoadPresentationFromXml(string filePath)
+        {
+            var xmlSerializer = new XmlSerializer(typeof(Presentation));
+
+            using (var stream = new FileStream(filePath, FileMode.Open))
+            using (var reader = new StreamReader(stream, System.Text.Encoding.UTF8))
+            {
+                var presentation = (Presentation)xmlSerializer.Deserialize(reader);
+
+                //ReplaceNewLineCharacters(presentation);
+
+                return presentation;
+            }
+        }
+
+
+        [HttpGet("/presentation")]
+        public IActionResult GetPresentation()
+        { return Ok(LoadPresentationFromXml("presentation.xml"));
+
+             
+
+            var presentationData = new Presentation
+            {
+                Title = "Moja Prezentacja",
+                Slides = new List<Slide>
+            {
+                new Slide
+                {
+                    Id = "1",
+                    Title = "Slajd 1",
+                    Elements = new List<Element>
+                    {
+                        new Element
+                        {
+                            Type = "text",
+                            Ops = new List<Op>
+                            {
+                                new Op { Insert = "Hello, " },
+                                new Op { Insert = "world", Attributes = new Attributes { Bold = true } },
+                                new Op { Insert = "!\nThis is a list:\nItem 1" },
+                                new Op { Insert = "\n", Attributes = new Attributes { List = "bullet" } },
+                                new Op { Insert = "Item 2" },
+                                new Op { Insert = "\n", Attributes = new Attributes { List = "bullet" } }
+                            }
+                        },
+                        new Element
+                        {
+                            Type = "image",
+                            Url = "/src/assets/deadpool.jpg"
+                        }
+                    }
+                },
+                new Slide
+                {
+                    Id = "2",
+                    Title = "Slajd 2",
+                    Elements = new List<Element>
+                    {
+                        new Element
+                        {
+                            Type = "image",
+                            Url = "/src/assets/deadpool.jpg"
+                        }
+                    }
+                }
+            }
+            };
+            SavePresentationToXml(presentationData, "presentation.xml");
+
+            return Ok(presentationData);
+        }
+
+     
+        [HttpPost("/presentation/save")]
+        public IActionResult SavePresentation([FromBody] Presentation presentation)
+        {
+            Debug.WriteLine("Zapisywanie...");
+            Debug.WriteLine(presentation.Slides[0].Elements[0].Ops[0].ToString);
+        
+
+
+            if (presentation == null)
+            {
+                return BadRequest("Prezentacja jest pusta.");
+            }
+            try
+            {
+                SavePresentationToXml(presentation, "presentation.xml");
+                Debug.WriteLine("Zapisano.");
+                return Ok($"Prezentacja została zapisana jako ");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Błąd podczas zapisywania prezentacji: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ Entities/Presentation/Presentation.cs
@@ -0,0 +1,135 @@
+﻿namespace EduCraftAPI.Entities.Presentation
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Xml.Serialization;
+    [XmlRoot("Presentation")]
+    public class Presentation
+    {
+        public string Title { get; set; }
+        [XmlArray("Slides")]
+        [XmlArrayItem("Slide")]
+        public List<Slide>? Slides { get; set; }
+    }
+    public class Slide
+    {
+        [XmlAttribute("Id")]
+        public string Id { get; set; }
+        public string? Title { get; set; } 
+        [XmlArray("Elements")]
+        [XmlArrayItem("Element")]
+        public List<Element>? Elements { get; set; } 
+    }
+
+    public class Element
+    {
+        public string? Type { get; set; }
+        public Position? Position { get; set; } = new Position(); 
+        public Size? Size { get; set; } = new Size(); 
+        [XmlArray("Ops")]
+        [XmlArrayItem("Op")]
+        public List<Op>? Ops { get; set; }
+        public string? Url { get; set; }
+        public override string ToString()
+        {
+            var properties = new List<string>();
+            properties.Add($"Type: {Type}");
+            properties.Add($"Position: {(Position != null ? Position.ToString() : "null")}");
+            properties.Add($"Size: {(Size != null ? Size.ToString() : "null")}");
+            if (Ops != null && Ops.Count > 0){
+                properties.Add("Ops: " + string.Join(", ", Ops));
+            }
+            else{
+                properties.Add("Ops: null or empty");
+            }
+            properties.Add($"Url: {Url}");
+            return string.Join(", ", properties);
+        }
+    }
+
+    public class Position
+    {
+        public float? Top { get; set; } = null;
+        public float? Left { get; set; } = null;
+    }
+    public class Size
+    {
+        public float? Width { get; set; } = null;
+        public float? Height { get; set; } = null;
+    }
+    public class Op
+    {
+        [XmlIgnore]
+        public string? Insert
+        {
+            get => _insert;
+            set
+            {
+                _insert = value;
+                _insertCode = value?.Replace("\n", "&#10;");
+            }
+        }
+       
+        private string? _insert;
+        private string? _insertCode;
+        [XmlElement("Insert")]
+        public string? InsertDecode
+        {
+            get => _insertCode;
+            set
+            {
+                _insertCode = value;
+                _insert = value?.Replace("&#10;", "\n");
+            }
+        }
+        public Attributes? Attributes { get; set; } = null;
+        public override string ToString()
+        {
+            if (Attributes == null)
+            {
+                return $"Insert: {Insert}";
+            }
+            return $"Insert: {Insert}, Attributes: {Attributes}";
+        }
+    }
+    public class Attributes
+    {
+        public bool? Bold { get; set; } 
+        public bool? Italic { get; set; } 
+        public bool? Underline { get; set; } 
+        public bool? Strike { get; set; } 
+        public bool? Blockquote { get; set; }
+        public int? Header { get; set; } 
+        public string? Script { get; set; } 
+        public int? Indent { get; set; } 
+        public string? Align { get; set; } 
+        public string? Direction { get; set; } 
+        public string? Size { get; set; } 
+        public string? Color { get; set; } 
+        public string? Background { get; set; } 
+        public string? Font { get; set; } 
+        public string? Link { get; set; } 
+        public string? List { get; set; }
+        public override string ToString()
+        {
+            var properties = new List<string>();
+            if (Bold.HasValue) properties.Add($"Bold: {Bold.Value}");
+            if (Italic.HasValue) properties.Add($"Italic: {Italic.Value}");
+            if (Underline.HasValue) properties.Add($"Underline: {Underline.Value}");
+            if (Strike.HasValue) properties.Add($"Strike: {Strike.Value}");
+            if (Blockquote.HasValue) properties.Add($"Blockquote: {Blockquote.Value}");
+            if (Header.HasValue) properties.Add($"Header: {Header.Value}");
+            if (Script != null) properties.Add($"Script: {Script}");
+            if (Indent.HasValue) properties.Add($"Indent: {Indent.Value}");
+            if (Align != null) properties.Add($"Align: {Align}");
+            if (Direction != null) properties.Add($"Direction: {Direction}");
+            if (Size != null) properties.Add($"Size: {Size}");
+            if (Color != null) properties.Add($"Color: {Color}");
+            if (Background != null) properties.Add($"Background: {Background}");
+            if (Font != null) properties.Add($"Font: {Font}");
+            if (Link != null) properties.Add($"Link: {Link}");
+            if (List != null) properties.Add($"List: {List}");
+            return string.Join(", ", properties);
+        }
+    }
+}
