272af8b5ae3d45b44c468e010b6b3d11950f83f5
Jakub-Okninski <jakub.okninski@onet.pl>
2024-11-16 21:45:16 +0100

    fileList

--- Controllers/FileController.cs
+++ Controllers/FileController.cs
@@ -1,21 +1,65 @@
 ﻿using EduCraftAPI.Data;
+using EduCraftAPI.Entities.Quiz;
+using EduCraftAPI.Models;
 using EduCraftAPI.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace EduCraftAPI.Controllers
 {
     public class FileController : Controller
     {
-
+        private readonly IUserContextService _userContextService;
         private readonly DataDbContext _context;
         private readonly IFileService _fileService;
 
-        public FileController(DataDbContext context, IFileService fileService)
+        public FileController(DataDbContext context, IFileService fileService, IUserContextService userContextService)
         {
             _context = context;
             _fileService = fileService;
+            _userContextService = userContextService;   
         }
+        [Authorize]
+        [HttpGet("/file/list")]
+        public IActionResult GetFlashcard()
+        {
+            var documents = _context.Flashcards
+              .Where(f => f.UserID == _userContextService.GetUserID)
+              .Select(f => new DocDTO()
+              {
+                  ID = f.FlashcardsID,
+                  Name = f.Title,
+                  Type = "Flashcards"
+              })
+              .Union(
+                  _context.Quizzes
+                      .Where(q => q.UserID == _userContextService.GetUserID)
+                      .Select(q => new DocDTO()
+                      {
+                          ID = q.QuizID,
+                          Name = q.Name,
+                          Type="Quiz"
+                      })
+              )
+              .Union(
+                  _context.Presentation
+                      .Where(p => p.UserID == _userContextService.GetUserID)
+                      .Select(p => new DocDTO()
+                      {
+                          ID = p.PresentationsID,
+                          Name = p.Title,
+                          Type = "Presentation"
+                      })
+              )
+            .ToList();
 
+            if (documents == null)
+            {
+                return NoContent();
+            }
+            return Ok(documents);
+        }
 
     }
 
---
+++ Models/DocDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace EduCraftAPI.Models
+{
+    public class DocDTO
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+
+    }
+}
